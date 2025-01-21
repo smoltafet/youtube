939ad4f3f962d48140cff1edb3b2b3f6cb543902
Sidiq Moltafet <sidiqmoltafet@gmail.com>
2025-01-21 15:15:45 -0800

    video processing config

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,34 @@
 import express from 'express';
-
+import ffmpeg from 'fluent-ffmpeg';
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-})
+app.post("/process-video", (req, res) => {
+  //Get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
 
+  if(!inputFilePath || !outputFilePath) {
+    res.status(400).send("Input file path and output file path are required");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") //360p converted video
+
+    .on("end", () => {
+      res.status(200).send("Video processing completed!");
+    })
+
+    .on("error", (error) => {
+      console.log("Error processing video", error);
+      res.status(500).send("Error processing video");
+    })
+
+    .save(outputFilePath);
+
+});
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
